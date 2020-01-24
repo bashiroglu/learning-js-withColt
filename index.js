fb7b1122290ab1079bcbe3bc34a8ee8bfde8a7df
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-24 22:56:29 +0100

    add isValidPassword function

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+// Write a isValidPassword function
+function isValidPassword(password, username) {
+  if (
+    password.length < 8 ||
+    password.includes(username) ||
+    password.includes(' ')
+  ) {
+    return false;
+  }
+  return true;
+}
 
+// const hello = 'contain hello';
+// console.log(hello.includes('contain'));
+// console.log(isValidPassword('hessen123', 'hesen'));
+console.log(isValidPassword('89Fjj1nms', 'dogLuvr')); //true);
+console.log(isValidPassword('dogLuvr123!', 'dogLuvr')); //false);
+console.log(isValidPassword('hello1', 'dogLuvr')); //false
+
+// It accepts 2 arguments: password and username
+// Password must:
+//	- be at least 8 characters
+//  - cannot contain spaces
+//  - cannot contain the username
+// If all requirements are met, return true.
+//Otherwise: false
